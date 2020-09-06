bb900807f481d23169686196de36d741b35ceae8
ArcadeTV <info@arcade-tv.de>
2020-09-06 23:02:13 +0200

    Add files via upload

--- _apply_srec-jp.bat
+++ _apply_srec-jp.bat
@@ -1,1 +1,1 @@
-srecpatch.exe roms/MICKEY_JP_UNTOUCHED_8M.BIN OUTPUT/CastleOfIllusion-MsuMD-JP.md<srecfile-jp.txt
+srecpatch.exe roms/MICKEY_JP_UNTOUCHED_16M.BIN OUTPUT/CastleOfIllusion-MsuMD-JP.md<srecfile-jp.txt
